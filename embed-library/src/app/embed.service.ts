9022ff2193c5102b69e4a6b3c8ed3359cf2d099d
dev <dev@devs-iMac-4.local>
2020-06-02 11:42:10 +0900

    add search

--- embed-library/src/app/embed.service.ts
+++ embed-library/src/app/embed.service.ts
@@ -10,20 +10,25 @@ export class EmbedService {
   private CYCServerURL = 'https://coreservices.hi-george.com/api/embed' // Static server url base path
   constructor(private http: HttpClient) { }
 
-  getEmbeds(): Observable<string[]>{
-  const url = this.CYCServerURL 
-  return this.http.get<any>(url).pipe(
-    catchError(this.handleError('getCampaign', null)),
-    
-  )
-}
-
-private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-    // TODO: send the error to remote logging infrastructure
-    console.error(error) // log to console instead
-    // Let the app keep running by returning an empty result.
-    return throwError(error.error.detail)
+  getEmbeds(searchParams: any = {}): Observable<string[]> {
+    console.log(searchParams)
+    const params = []
+    for (let key in searchParams) {
+      if (!searchParams[key]) continue
+      params.push(`${key}=${searchParams[key]}`)
+    }
+    const url = `${this.CYCServerURL}?${params.join('&')}`
+    return this.http.get<any>(url).pipe(
+      catchError(this.handleError('getCampaign', null)),
+    )
   }
-}
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error) // log to console instead
+      // Let the app keep running by returning an empty result.
+      return throwError(error.error.detail)
+    }
+  }
 }
