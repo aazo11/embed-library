ed986a9be9aaf6c17685eb0e4ea28ece8e658f8c
dev <dev@devs-iMac-4.local>
2020-06-22 10:40:33 +0900

    update ui

--- embed-library/src/app/app-routing.module.ts
+++ embed-library/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {WidgetLibraryComponent} from 'src/app/widget-library/widget-library.component'
+import { WidgetLibraryComponent } from 'src/app/components/widget-library/widget-library.component'
 
 
 const routes: Routes = [
---
+++ embed-library/src/app/components/widget/widget.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatDialog } from '@angular/material/dialog';
+import { WidgetDialogComponent } from 'src/app/components/widget-dialog/widget-dialog.component';
+
+@Component({
+  selector: 'app-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['./widget.component.scss']
+})
+export class WidgetComponent implements OnInit {
+  @Input() campaign: any
+
+  embedCodeHTML: string
+
+  server_url = "https://callyourcongressperson.com/embed/"
+  iframeSRC: string;
+
+  constructor(
+    public sanitizer: DomSanitizer,
+    private dialog: MatDialog
+  ) { }
+
+  ngOnInit(): void {
+    this.iframeSRC = this.server_url + this.campaign.id
+    this.embedCodeHTML = '<iframe style="display:block;" width="665px" height="530px" src="' + this.iframeSRC + '" frameborder="0"></iframe>'
+  }
+
+  getCodeForWidget(): void {
+    // this.copyEmbedCode.emit(this.campaign.embedTitleInLibrary)
+  }
+
+  openDialog(): void {
+    this.dialog.open(WidgetDialogComponent, {
+      width: '520px',
+      data: { src: this.iframeSRC },
+    });
+  }
+}
