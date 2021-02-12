25548cc6aa6d4f8d8e2b7947a4adb0d1a0817611
Saburo <suzuki.saburo16@gmail.com>
2021-02-13 06:44:31 +0900

    add clickthrough-agreeemnt

--- embed-library/src/app/app-routing.module.ts
+++ embed-library/src/app/app-routing.module.ts
@@ -1,36 +1,44 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { HomeComponent } from 'src/app/pages/home/home.component';
-import { HowItWorksComponent } from 'src/app/pages/how-it-works/how-it-works.component';
-import { WidgetLibraryComponent } from 'src/app/pages/widget-library/widget-library.component';
-import { CompanyComponent } from 'src/app/pages/company/company.component';
-import { ContactComponent } from 'src/app/pages/contact/contact.component';
-import { PreviewComponent } from 'src/app/pages/preview/preview.component';
-import { CaseStudyComponent } from 'src/app/pages/case-study/case-study.component';
-import { PrivacyComponent } from 'src/app/pages/privacy/privacy.component';
-import { TosComponent } from 'src/app/pages/tos/tos.component';
-import { DemoComponent } from 'src/app/pages/demo/demo.component';
-import { SelfserveComponent } from 'src/app/pages/selfserve/selfserve.component';
+import { HomeComponent } from "src/app/pages/home/home.component";
+import { HowItWorksComponent } from "src/app/pages/how-it-works/how-it-works.component";
+import { WidgetLibraryComponent } from "src/app/pages/widget-library/widget-library.component";
+import { CompanyComponent } from "src/app/pages/company/company.component";
+import { ContactComponent } from "src/app/pages/contact/contact.component";
+import { PreviewComponent } from "src/app/pages/preview/preview.component";
+import { CaseStudyComponent } from "src/app/pages/case-study/case-study.component";
+import { PrivacyComponent } from "src/app/pages/privacy/privacy.component";
+import { TosComponent } from "src/app/pages/tos/tos.component";
+import { DemoComponent } from "src/app/pages/demo/demo.component";
+import { SelfserveComponent } from "src/app/pages/selfserve/selfserve.component";
+import { ClickthroughAgreementComponent } from "src/app/pages/clickthrough-agreement/clickthrough-agreement.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'how-it-works', component: HowItWorksComponent },
-  { path: 'visualizations', component: WidgetLibraryComponent },
-  { path: 'company', component: CompanyComponent },
-  { path: 'advisors', redirectTo: '/company#advisors' },
-  { path: 'contact', component: ContactComponent },
-  { path: 'preview', component: PreviewComponent },
-  { path: 'preview/:collection', component: PreviewComponent },
-  { path: 'case-study/:brand', component: CaseStudyComponent },
-  { path: 'privacy', component: PrivacyComponent },
-  { path: 'demo', component: DemoComponent },
-  { path: 'selfserve', component: SelfserveComponent },
-  { path: 'tos', component: TosComponent }
+  { path: "", component: HomeComponent },
+  { path: "how-it-works", component: HowItWorksComponent },
+  { path: "visualizations", component: WidgetLibraryComponent },
+  { path: "company", component: CompanyComponent },
+  { path: "advisors", redirectTo: "/company#advisors" },
+  { path: "contact", component: ContactComponent },
+  { path: "preview", component: PreviewComponent },
+  { path: "preview/:collection", component: PreviewComponent },
+  { path: "case-study/:brand", component: CaseStudyComponent },
+  { path: "privacy", component: PrivacyComponent },
+  { path: "demo", component: DemoComponent },
+  { path: "selfserve", component: SelfserveComponent },
+  { path: "tos", component: TosComponent },
+  { path: "clickthrough-agreement", component: ClickthroughAgreementComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', initialNavigation: 'enabled' })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: "enabled",
+      anchorScrolling: "enabled",
+      initialNavigation: "enabled",
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- embed-library/src/app/app.module.ts
+++ embed-library/src/app/app.module.ts
@@ -1,47 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from 'src/app/pages/home/home.component';
-import { HowItWorksComponent } from 'src/app/pages/how-it-works/how-it-works.component';
-import { CompanyComponent } from 'src/app/pages/company/company.component';
-import { ContactComponent } from 'src/app/pages/contact/contact.component';
-import { WidgetLibraryComponent } from 'src/app/pages/widget-library/widget-library.component';
-import { PreviewComponent } from 'src/app/pages/preview/preview.component';
-import { CaseStudyComponent } from 'src/app/pages/case-study/case-study.component';
-import { PrivacyComponent } from 'src/app/pages/privacy/privacy.component';
-import { TosComponent } from 'src/app/pages/tos/tos.component';
-import { DemoComponent } from 'src/app/pages/demo/demo.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "src/app/pages/home/home.component";
+import { HowItWorksComponent } from "src/app/pages/how-it-works/how-it-works.component";
+import { CompanyComponent } from "src/app/pages/company/company.component";
+import { ContactComponent } from "src/app/pages/contact/contact.component";
+import { WidgetLibraryComponent } from "src/app/pages/widget-library/widget-library.component";
+import { PreviewComponent } from "src/app/pages/preview/preview.component";
+import { CaseStudyComponent } from "src/app/pages/case-study/case-study.component";
+import { PrivacyComponent } from "src/app/pages/privacy/privacy.component";
+import { TosComponent } from "src/app/pages/tos/tos.component";
+import { DemoComponent } from "src/app/pages/demo/demo.component";
+import { ClickthroughAgreementComponent } from "src/app/pages/clickthrough-agreement/clickthrough-agreement.component";
 
-import { WidgetComponent } from 'src/app/components/widget/widget.component';
-import { SearchComponent } from 'src/app/components/search/search.component';
-import { WidgetDialogComponent } from 'src/app/components/widget-dialog/widget-dialog.component';
-import { HeaderComponent } from 'src/app/components/header/header.component';
-import { FooterComponent } from 'src/app/components/footer/footer.component';
-import { ButtonComponent } from 'src/app/components/button/button.component';
-import { TouchPanelComponent } from 'src/app/components/touch-panel/touch-panel.component';
-import { ContactSuccessDialogComponent } from 'src/app/components/contact-success-dialog/contact-success-dialog.component';
-import { LocationSearchComponent } from 'src/app/components/location-search/location-search.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
+import { WidgetComponent } from "src/app/components/widget/widget.component";
+import { SearchComponent } from "src/app/components/search/search.component";
+import { WidgetDialogComponent } from "src/app/components/widget-dialog/widget-dialog.component";
+import { HeaderComponent } from "src/app/components/header/header.component";
+import { FooterComponent } from "src/app/components/footer/footer.component";
+import { ButtonComponent } from "src/app/components/button/button.component";
+import { TouchPanelComponent } from "src/app/components/touch-panel/touch-panel.component";
+import { ContactSuccessDialogComponent } from "src/app/components/contact-success-dialog/contact-success-dialog.component";
+import { LocationSearchComponent } from "src/app/components/location-search/location-search.component";
+import { UserFormComponent } from "./components/user-form/user-form.component";
 
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCardModule } from '@angular/material/card';
-import { MatSnackBarModule } from '@angular/material/snack-bar'
-import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { ClipboardModule } from 'ngx-clipboard';
-import { CachedSrcDirective } from './cached-src.directive';
-import { SelfserveComponent } from './pages/selfserve/selfserve.component';
-import { PopupComponent } from './components/popup/popup.component';
-import { SelfserveSearchComponent } from './components/selfserve-search/selfserve-search.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatSelectModule } from "@angular/material/select";
+import { MatCardModule } from "@angular/material/card";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { ClipboardModule } from "ngx-clipboard";
+import { CachedSrcDirective } from "./cached-src.directive";
+import { SelfserveComponent } from "./pages/selfserve/selfserve.component";
+import { PopupComponent } from "./components/popup/popup.component";
+import { SelfserveSearchComponent } from "./components/selfserve-search/selfserve-search.component";
 
 @NgModule({
   declarations: [
@@ -56,6 +58,7 @@ import { SelfserveSearchComponent } from './components/selfserve-search/selfserv
     PrivacyComponent,
     TosComponent,
     DemoComponent,
+    ClickthroughAgreementComponent,
 
     HeaderComponent,
     FooterComponent,
@@ -70,10 +73,10 @@ import { SelfserveSearchComponent } from './components/selfserve-search/selfserv
     CachedSrcDirective,
     SelfserveComponent,
     PopupComponent,
-    SelfserveSearchComponent
+    SelfserveSearchComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({ appId: "serverApp" }),
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
@@ -81,6 +84,7 @@ import { SelfserveSearchComponent } from './components/selfserve-search/selfserv
     MatCardModule,
     MatSnackBarModule,
     MatInputModule,
+    MatCheckboxModule,
     MatSelectModule,
     MatIconModule,
     MatDialogModule,
@@ -90,11 +94,8 @@ import { SelfserveSearchComponent } from './components/selfserve-search/selfserv
     HttpClientModule,
     ClipboardModule,
   ],
-  entryComponents: [
-    WidgetDialogComponent,
-    ContactSuccessDialogComponent
-  ],
+  entryComponents: [WidgetDialogComponent, ContactSuccessDialogComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
