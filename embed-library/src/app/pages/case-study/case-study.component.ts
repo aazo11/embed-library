be7f8261ead6ccad89d7f484a7cc329dde62a85d
Saburo <suzuki.saburo16@gmail.com>
2021-02-19 02:01:53 +0900

    update home carousel

--- embed-library/src/app/pages/case-study/case-study.component.ts
+++ embed-library/src/app/pages/case-study/case-study.component.ts
@@ -1,28 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { contents } from './constants';
-import { AnalyticsService } from 'src/app/analytics.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { contents } from "./constants";
+import { AnalyticsService } from "src/app/analytics.service";
 
 @Component({
-  selector: 'app-case-study',
-  templateUrl: './case-study.component.html',
-  styleUrls: ['./case-study.component.scss']
+  selector: "app-case-study",
+  templateUrl: "./case-study.component.html",
+  styleUrls: ["./case-study.component.scss"],
 })
-export class CaseStudyComponent implements OnInit{
-  brand: string
+export class CaseStudyComponent implements OnInit {
+  brand: string;
 
   constructor(
     private route: ActivatedRoute,
     private analytics: AnalyticsService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.brand = this.route.snapshot.params.brand;
   }
 
   get content(): any {
-    console.log(this.brand)
-    return contents[this.brand]
+    return contents[this.brand];
   }
 
   imageUrl(title): string {
@@ -30,6 +29,6 @@ export class CaseStudyComponent implements OnInit{
   }
 
   trackClick() {
-    this.analytics.trackLink('view_article', this.brand);
+    this.analytics.trackLink("view_article", this.brand);
   }
 }
