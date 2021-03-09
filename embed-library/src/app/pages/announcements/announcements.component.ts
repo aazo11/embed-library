13e71bade43a6491021bba05e1fbbddc0e4178d8
Amir A. Zohrenejad <aazo11@gmail.com>
2021-03-08 18:42:24 -0800

    fix bugs. Add avatar. Add meta tags

--- embed-library/src/app/pages/announcements/announcements.component.ts
+++ embed-library/src/app/pages/announcements/announcements.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-announcements',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnouncementsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private meta: Meta) { }
+  IMAGEURL:string = "https://hi-george.s3.amazonaws.com/random/YC.png"
 
   ngOnInit(): void {
+    this.meta.addTag({name: 'twitter:card', content: 'summary_large_image'});
+    this.meta.addTag({name: 'twitter:title', content: 'YC Funds HiGeorge'})
+    this.meta.addTag({name: 'og:title', content: 'YC Funds HiGeorge'})
+    this.meta.addTag({name: 'twitter:image', content: this.IMAGEURL })
+    this.meta.addTag({name: 'og:image', content:this.IMAGEURL})
   }
 
 }
